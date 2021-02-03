6cefcf688ebd79d9db1a0b875235835a166431ee
Charles Wei <javacs3+aws@gmail.com>
2021-02-03 16:00:48 +0800

    chore: add S005 lambda

--- docs/README.md
+++ docs/README.md
@@ -73,3 +73,49 @@ new MyStack(app, 'Stack', {
 ```
 <small>more details [S004-Do-not-hardcode-env.ts](../src/stacks/S004-Do-not-hardcode-env.ts)</small>
 
+### S005: Lambda layer
+```
+.
+├── index.ts
+└── lambda/
+    ├── package-lock.json
+    ├── package.json
+    └── src/
+        └── index.js*
+
+2 directories, 4 files
+```
+```ts
+const layer = new lambda.LayerVersion(this, 'MyLayer', {
+  code: lambda.Code.fromAsset(path.join(__dirname, './lambda/'), {
+    bundling: {
+      image: lambda.Runtime.NODEJS_12_X.bundlingDockerImage,
+      command: [
+        'bash', '-xc', [
+          'export npm_config_update_notifier=false',
+          'export npm_config_cache=$(mktemp -d)', // https://github.com/aws/aws-cdk/issues/8707#issuecomment-757435414
+          'cd $(mktemp -d)',
+          'cp -v /asset-input/package*.json .',
+          'npm i --only=prod',
+          'mkdir -p /asset-output/nodejs/',
+          'cp -au node_modules /asset-output/nodejs/',
+        ].join('&&'),
+      ],
+    },
+  }),
+  compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
+  description: 'A layer to test the L2 construct',
+});
+
+new lambda.Function(this, 'MyHandler', {
+  runtime: lambda.Runtime.NODEJS_12_X,
+  code: lambda.Code.fromAsset(path.join(__dirname, './lambda/src')),
+  handler: 'index.handler',
+  layers: [layer],
+});
+```
+<small>more details [index.ts](../src/stacks/S005-Lambda-layer/index.ts)</small>
+
+
+
+
--- scripts/mkdoc.py
+++ scripts/mkdoc.py
@@ -3,17 +3,23 @@ import os
 import re
 import sys
 import textwrap
+import subprocess
+
+
+def sh(*args):
+    return subprocess.check_output(*args, shell=True).decode()
 
 
 def cat(*s, delimiter='\n'):
     return delimiter.join(s)
 
 
-def render(filename, basedir=os.curdir):
+def renderf(filename, basedir=os.curdir):
     lang = os.path.splitext(filename)[1][1:]
     show = False
     head = []
     desc = []
+    tree = []
     body = []
     with open(filename) as fp:
         for line in fp:
@@ -22,6 +28,13 @@ def render(filename, basedir=os.curdir):
                 continue
             if re.search(r'^\s*/// !hide', line, re.MULTILINE):
                 show = False
+                continue
+            if re.search(r'^\s*/// !tree', line, re.MULTILINE):
+                tree = [
+                    '```',
+                    sh(f'cd {os.path.dirname(filename)} && tree -L 3 -F .').rstrip(),
+                    '```',
+                ]
                 continue
             m = re.search(r'^\s*/// !title\s+(.*)', line, re.MULTILINE)
             if m:
@@ -36,6 +49,7 @@ def render(filename, basedir=os.curdir):
     return cat(
         *head,
         *desc,
+        *tree,
         *(
             f'```{lang}',
             textwrap.dedent(cat(*body)),
@@ -46,12 +60,25 @@ def render(filename, basedir=os.curdir):
     )
 
 
+def _render(path, basedir=os.curdir, level=3):
+    if os.path.isfile(path):
+        yield renderf(path, basedir)
+    if os.path.isdir(path) and (level > 0):
+        for p in map(lambda f: os.path.join(path, f), sorted(os.listdir(path))):
+            for s in _render(p, basedir, level-1):
+                yield s
+
+
+def render(path, basedir=os.curdir, level=3):
+    return cat(*_render(path, basedir, level))
+
+
 def main():
     dir = os.path.abspath(sys.argv[1])
     out = os.path.abspath(sys.argv[2])
 
     files = map(lambda f: os.path.join(dir, f), sorted(os.listdir(dir)))
-    with open(out, 'w+') as fp:
+    with open(out, 'w') as fp:
         for each in files:
             fp.write(render(each, basedir=os.path.dirname(out)))
             fp.write('\n')
